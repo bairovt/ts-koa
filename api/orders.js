922b1799e54049a05139f0a979b8a29aaf992221
Tumen Bairov <bairovt@gmail.com>
2019-08-26 05:41:06 +0900

    some changes

--- api/orders.js
+++ api/orders.js
@@ -7,18 +7,18 @@ const _ = require('lodash');
 
 const router = new Router();
 
-const orderSchema = Joi.object().keys({
-  name: Joi.string().trim().min(2).max(100).required(),
-  tel: Joi.string().trim().min(5).max(20).empty('').required(),
-  place: Joi.string().trim().min(1).max(255).empty('').allow(null),
-  comment: Joi.string().trim().min(1).max(3000).empty('').allow(null),
-  // createdBy: Joi.string().trim().regex(/^[a-zA-Z0-9]+$/).min(3).max(30).allow(null),
-});
+// const orderSchema = Joi.object().keys({
+//   name: Joi.string().trim().min(2).max(100).required(),
+//   tel: Joi.string().trim().min(5).max(20).empty('').required(),
+//   place: Joi.string().trim().min(1).max(255).empty('').allow(null),
+//   comment: Joi.string().trim().min(1).max(3000).empty('').allow(null),
+//   // createdBy: Joi.string().trim().regex(/^[a-zA-Z0-9]+$/).min(3).max(30).allow(null),
+// });
 
-async function getOrders(ctx, next) {
+async function filterOrders(ctx, next) {
   let orders = await db.query(aql`FOR order IN Orders
   // FILTER rod.type == 'subethnos'
-  // SORT rod.order DESC
+  SORT order.date ASC
   RETURN order`).then(cursor => cursor.all());
   ctx.body = {
     orders
@@ -30,18 +30,19 @@ async function getOrder(ctx, next) {
     _key
   } = ctx.params;
   const ordersCollection = db.collection('Orders');
-  order = await ordersCollection.document(_key);
+  const order = await ordersCollection.document(_key);
   ctx.body = {
     order
   };
 }
 
-async function addOrder(ctx) {
+async function createOrder(ctx) {
   const ordersCollection = db.collection('Orders');
   const {
     orderData
   } = ctx.request.body;
-  const validOrder = Joi.attempt(orderData, orderSchema);
+  // const validOrder = Joi.attempt(orderData, orderSchema);
+  const validOrder = orderData;
   validOrder.createdBy = ctx.state.user._id;
   validOrder.createdAt = new Date();
   const result = await ordersCollection.save(validOrder, {
@@ -69,12 +70,12 @@ async function updateOrder(ctx) { // key
   } = ctx.params;
   const {
     orderData
-  } = ctx.body;
+  } = ctx.request.body;
   const ordersCollection = db.collection('Orders');
   const order = ordersCollection.document(_key);
   if (!order) ctx.throw(404, 'Document not found');
   _.merge(order, orderData);
-  Joi.attempt(order, orderSchema);
+  // Joi.attempt(order, orderSchema);
   orderData.updatedAt = new Date();
   orderData.updatedBy = ctx.state.user._id;
   await ordersCollection.update(_key, orderData);
@@ -82,10 +83,10 @@ async function updateOrder(ctx) { // key
 }
 
 router
-  .get('/', getOrders)
-  .post('/', addOrder)
-  .get('/:key', getOrder)
-  .delete('/:key', deleteOrder)
-  .patch('/:key', updateOrder)
+  .get('/', filterOrders)
+  .post('/', createOrder)
+  .get('/:_key', getOrder)
+  .delete('/:_key', deleteOrder)
+  .patch('/:_key', updateOrder)
 
 module.exports = router.routes();
